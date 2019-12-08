62c5ccc6e6c44b74486274949a478c27e045c8f9
robyafrizal <robyafrizal86@gmail.com>
2019-12-08 16:58:54 +0700

    Edit Home route

--- src/App.js
+++ src/App.js
@@ -2,22 +2,9 @@ import React from "react";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { MDBBtn, MDBContainer, MDBRow, MDBCol, MDBCardImage } from "mdbreact";
-
 const App = () => (
   <div>
     <Navbar />
-    <MDBContainer className="mt-5 text-center">
-      <MDBRow>
-        <MDBCol>
-          <MDBCardImage
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mELUH0EqYeiry3LpUWN9lxVh3Lr6YbeAXkf59JvJavxcnyRbCQ&s"
-            className="img w-100"
-          />
-          <MDBBtn color="warning">Find Laundry Store</MDBBtn>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
     <Footer />
   </div>
 );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { MDBBtn, MDBContainer, MDBRow, MDBCol, MDBCardImage } from "mdbreact";
 
 const Home = () => {
   return (
     <div>
-      <h1>This is HomePage</h1>
+      <MDBContainer className="mt-5 text-center">
+        <MDBRow>
+          <MDBCol>
+            <MDBCardImage
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mELUH0EqYeiry3LpUWN9lxVh3Lr6YbeAXkf59JvJavxcnyRbCQ&s"
+              className="img w-100"
+            />
+            <MDBBtn color="warning">Find Laundry Store</MDBBtn>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
     </div>
   );
 };
