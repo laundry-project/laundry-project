068fe06e3faa337aa7474178b1d8fe40bd060cac
Dianpuspitasari86 <dianpuspitasari171586>
2019-12-09 14:30:03 +0700

    Edit Find1

--- src/components/FindLaundry.js
+++ src/components/FindLaundry.js
@@ -1,62 +1,88 @@
 import React from "react";
-import { withRouter } from "react-router";
-import { Button, Card, Image, Modal } from "semantic-ui-react";
 import logo from "../components/images/fresh.jpg";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import "semantic-ui-css/semantic.min.css";
+import { MDBRow, MDBCol, MDBIcon, MDBBtn } from "mdbreact";
 
+const FindLaundry = () => {
+  return (
+    <div>
+      <Navbar />
+      <section className="my-5">
+        <h1 className="h1-responsive font-weight-bold text-center my-5">
+       Temukan Cabang Fresh and Clean Laundry Terdekat
+        </h1>
+        <p className="lead grey-text w-responsive text-center mx-auto mb-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam.
+        </p>
 
-const FindLaundry = () => (
-  <div>
-    <Navbar />
-    <h1>Temukan Cabang Fresh and Clean Laundry Terdekat</h1>
-    <Card.Group>
-      <Card>
-        <Card.Content>
-          <Image src={logo} />
-          <Card.Header>Fresh and Clean laundry - Kemang</Card.Header>
-          <Card.Description>Fresh Laundry Cabang Kemang</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <div className="ui two buttons">
-            <Modal
-              trigger={<Button basic color="green">Info</Button>}
-            ></Modal>
-          </div>
-        </Card.Content>
-      </Card>
-      <Card>
-        <Card.Content>
-          <Image src={logo} />
-          <Card.Header>Fresh laundry - Tebet</Card.Header>
-          <Card.Description>Fresh and Clean Laundry Cabang Tebet</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <div className="ui two buttons">
-            <Button basic color="green">
-              Info
-            </Button>
-          </div>
-        </Card.Content>
-      </Card>
-      <Card>
-        <Card.Content>
-          <Image src={logo} />
-          <Card.Header>Fresh laundry - Grogol</Card.Header>
-          <Card.Description>Fresh and Clean Laundry Cabang Grogol</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <div className="ui two buttons">
-            <Button basic color="green">
-              Info
-            </Button>
-          </div>
-        </Card.Content>
-      </Card>
-    </Card.Group>
-    <Footer />
-  </div>
-);
+        <MDBRow>
+          <MDBCol md="4" className="md-0 mb-5">
+            <MDBRow>
+              <MDBCol lg="2" md="3" size="2">
+                {/* <MDBIcon icon="bullhorn" size="2x" className="blue-text" /> */}
+              </MDBCol>
+              <MDBCol lg="10" md="9" size="10">
+                <h2 className="font-weight-bold">Fresh and Clean Laundry Kemang</h2>
+                <p className="grey-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam.
+                </p>
+                <MDBBtn color="primary" size="sm">
+                  Learn more
+                </MDBBtn>
+              </MDBCol>
+            </MDBRow>
+          </MDBCol>
+          <MDBCol md="4" className="md-0 mb-5">
+            <MDBRow>
+              <MDBCol lg="2" md="3" size="2">
+                <MDBIcon icon="cogs" size="2x" className="pink-text" />
+              </MDBCol>
+              <MDBCol lg="10" md="9" size="10">
+                <h2 className="font-weight-bold">Fresh and Clean Laundry Tebet</h2>
+                <p className="grey-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam.
+                </p>
+                <MDBBtn color="pink" size="sm">
+                  Learn more
+                </MDBBtn>
+              </MDBCol>
+            </MDBRow>
+          </MDBCol>
+          <MDBCol md="4" className="md-0 mb-5">
+            <MDBRow>
+              <MDBCol lg="2" md="3" size="2">
+                <MDBIcon
+                  icon="tachometer-alt"
+                  size="2x"
+                  className="purple-text"
+                />
+              </MDBCol>
+              <MDBCol lg="10" md="9" size="10">
+                <h2 className="font-weight-bold">Fresh and Clean Laundry Grogol</h2>
+                <p className="grey-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam.
+                </p>
+                <MDBBtn color="purple" size="sm">
+                  Learn more
+                </MDBBtn>
+              </MDBCol>
+            </MDBRow>
+          </MDBCol>
+        </MDBRow>
+      </section>
+      <Footer />
+    </div>
+  );
+};
 
-export default withRouter(FindLaundry);
+export default FindLaundry;
