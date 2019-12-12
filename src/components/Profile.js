cbf6a132ac2776caac2a0aeb55fb403b4e47e6de
Dianpuspitasari86 <dianpuspitasari171586>
2019-12-12 10:46:14 +0700

    Edit About Us

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import NavbarAfterlog from './NavbarAfterlog'
 import Team from "./Team";
 import Footer from './Footer';
+import Navbar from "./Navbar";
 import Icon from "./Icon";
 
 export default function Profile() {
     return (
         <div>
-        <NavbarAfterlog/>
+        <Navbar />
         <Team/>
         <Icon/>
         <Footer/>
