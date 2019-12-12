dd2afa24d89bf9831af9ef1ae6f4abfd23b15f9b
taufik66645 <taufik.66645@gmail.com>
2019-12-12 10:39:36 +0700

    Modifying

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import NavbarAfterlog from "./NavbarAfterlog";
-import { Table } from "semantic-ui-react";
-import { Card, Image } from "semantic-ui-react";
-
-const TableExamplePagination = () => (
-  <div>
-    <NavbarAfterlog />
-    <Card>
-      <Image ui={false} />
-      <Card.Content>
-        <Card.Header>Taufik Hidayat</Card.Header>
-        <Card.Meta>Bergabung pada 11 Desember 2019</Card.Meta>
-        <Card.Description>Email : taufik@mailcom</Card.Description>
-        <Card.Description>Password : 12345</Card.Description>
-        <Card.Description>Alamat : Jakarta</Card.Description>
-      </Card.Content>
-    </Card>
-    <h1>History Pemesanan</h1>
-    <Table celled>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>Tanggal</Table.HeaderCell>
-          <Table.HeaderCell>Order Id</Table.HeaderCell>
-          <Table.HeaderCell>Pemesanan</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-
-      <Table.Body>
-        <Table.Row>
-          <Table.Cell>Senin, 22 Maret 2019</Table.Cell>
-          <Table.Cell>Order-Id-01</Table.Cell>
-          <Table.Cell>Cuci Kiloan</Table.Cell>
-        </Table.Row>
-      </Table.Body>
-    </Table>
-  </div>
-);
-
-export default TableExamplePagination;
-=======
 import React from 'react'
 import NavbarAfterlog from './NavbarAfterlog'
 import Team from "./Team";
@@ -57,4 +15,3 @@ export default function Profile() {
     )
    
 }
->>>>>>> master
