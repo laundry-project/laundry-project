90976d7558356e8f9e7008d73b0dadf62733b45c
Dianpuspitasari86 <dianpuspitasari171586>
2019-12-12 10:23:59 +0700

    Edit profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import NavbarAfterlog from './NavbarAfterlog'
+import Team from "./Team";
+import Footer from './Footer';
+import Icon from "./Icon";
 
 export default function Profile() {
     return (
         <div>
         <NavbarAfterlog/>
+        <Team/>
+        <Icon/>
+        <Footer/>
                 </div>
     )
+   
 }
