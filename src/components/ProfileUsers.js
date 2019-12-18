9d04f70ff0e9b36ee8f3ae4cfbfd4f22b2fe5b00
taufik66645 <taufik.66645@gmail.com>
2019-12-18 16:15:32 +0700

    edit history

--- src/components/ProfileUsers.js
+++ src/components/ProfileUsers.js
@@ -5,6 +5,7 @@ import { ServiceContext } from "./ServiceContext";
 import axios from "axios";
 
 import {
+  MDBBtn,
   MDBCard,
   MDBCardBody,
   MDBCardImage,
@@ -14,7 +15,11 @@ import {
   MDBCol,
   MDBTable,
   MDBTableBody,
-  MDBTableHead
+  MDBTableHead,
+  MDBModal,
+  MDBModalBody,
+  MDBModalHeader,
+  MDBModalFooter
 } from "mdbreact";
 
 const URI = process.env.REACT_APP_API_URI;
@@ -22,6 +27,11 @@ const URI = process.env.REACT_APP_API_URI;
 function ProfileUser() {
   let [userContext, setUserContext] = useContext(UserContext);
   let [order, setOrder] = useState([]);
+  let [modal, setModal] = useState(false);
+
+  const toggle = () => {
+    setModal(!modal);
+  };
 
   useEffect(() => {
     axios
@@ -42,54 +52,67 @@ function ProfileUser() {
   console.log(order);
 
   return (
-    <MDBRow className="mx-auto w-50 mt-5">
-      <MDBCol className="text-center">
-        <MDBCard narrow>
-          <center>
-            <MDBCardImage
-              cascade
-              className="img-fluid my-4 w-50 h-50 rounded-circle"
-              src="https://mdbootstrap.com/img/Photos/Avatars/img(30).jpg"
-            />
-          </center>
-          <MDBCardBody cascade>
-            <MDBCardTitle>
-              <h1>Personal Information</h1>
-            </MDBCardTitle>
-            <MDBCardText>
-              <h3>Name : {userContext.name}</h3>
-              <h3>Email : {userContext.email}</h3>
-              <h3>Telephone : {userContext.telephone}</h3>
+    <div>
+      <MDBRow className="mx-auto w-50 mt-5">
+        <MDBCol className="text-center">
+          <MDBCard narrow>
+            <center>
+              <MDBCardImage
+                cascade
+                className="img-fluid my-4 w-50 h-50 rounded-circle"
+                src="http://cdn.onlinewebfonts.com/svg/img_237553.png"
+              />
+            </center>
+            <MDBCardBody cascade>
+              <MDBCardTitle>
+                <h1>Personal Information</h1>
+              </MDBCardTitle>
+              <MDBCardText>
+                <h3>Name : {userContext.name}</h3>
+                <h3>Email : {userContext.email}</h3>
+                <h3>Telephone : {userContext.telephone}</h3>
+              </MDBCardText>
+            </MDBCardBody>
 
-              <MDBTable bordered>
-                <MDBTableHead color="primary-color">
-                  <tr>
-                    <th>Tanggal</th>
-                    <th>Service</th>
-                    <th>Amount</th>
-                    <th>Price</th>
-                    <th>Address</th>
-                  </tr>
-                </MDBTableHead>
-                <MDBTableBody>
-                  {order.map((item, index) => (
-                    <tr key={index}>
-                      <td>{item.date}</td>
-                      <td>{item.service}</td>
-                      <td>{item.amount}</td>
-                      <td>{item.price}</td>
-                      <td>{item.address}</td>
-                    </tr>
-                  ))}
-                </MDBTableBody>
-              </MDBTable>
-            </MDBCardText>
-          </MDBCardBody>
-        </MDBCard>
-      </MDBCol>
-    </MDBRow>
+            <MDBBtn color="primary" onClick={toggle}>
+              View History
+            </MDBBtn>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+
+      <MDBModal isOpen={modal} toggle={toggle}>
+        <MDBModalHeader toggle={toggle}>Service History</MDBModalHeader>
+        <MDBModalBody>
+          <MDBTable bordered className="text-center">
+            <MDBTableHead>
+              <tr>
+                <th>Date</th>
+                <th>Service</th>
+                <th>Amount</th>
+                <th>Price</th>
+              </tr>
+            </MDBTableHead>
+            <MDBTableBody>
+              {order.map(item => (
+                <tr>
+                  <td>{item.date}</td>
+                  <td>{item.service}</td>
+                  <td>{item.amount}</td>
+                  <td>{item.price}</td>
+                </tr>
+              ))}
+            </MDBTableBody>
+          </MDBTable>
+        </MDBModalBody>
+        <MDBModalFooter>
+          <MDBBtn color="primary" onClick={toggle}>
+            Close
+          </MDBBtn>
+        </MDBModalFooter>
+      </MDBModal>
+    </div>
   );
-  // }
 }
 
 export default ProfileUser;
